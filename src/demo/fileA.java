aae981ac12bae0f77559538665a4343cf9556a0b
Miracle- <2632687880@qq.com>
2023-04-15 21:34:27 +0800

    demo-commit

---
+++ src/demo/fileA.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * 　　　　* @author ZT
+ * 　　　　* @date 2023/4/15 20:29
+ **/
+public class fileA {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
---
+++ src/demo/fileB.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * 　　　　* @author ZT
+ * 　　　　* @date 2023/4/15 20:31
+ **/
+public class fileB {
+    public static void main(String[] args) {
+        System.out.println("BBB");
+    }
+}
